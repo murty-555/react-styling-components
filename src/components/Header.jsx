5b19deeff51f89fed8496c2e5451fc313a3a12bb
murty-555 <krlvmurthy96@gmail.com>
2024-02-09 17:33:24 +0530

    using styled components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import logo from "../assets/logo.png";
+// import {styled} from 'styled-components';
+
 import './Header.css'
+
+// const StyledComp = styled.div`
+//     width: 50%;
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+// `
+
 
 export default function Header(){
     return (
         <header>
+            {/* <StyledComp>
+                <button>item1</button>
+                <button>item1</button>
+            </StyledComp> */}
             <img src={logo} alt="A canvas"/>
             <h1>ReactArt</h1>
             <p>A community of artists and art-lovers.</p>
